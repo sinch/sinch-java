d379e7e1b355150732ab024b62db6237f94f4762
Fredrik Kratzer <fredrik.kratzer@sinch.com>
2021-03-24 14:15:43 +0100

    Add support for adding multiple recipient identities to a message request

--- src/main/java/com/sinch/sdk/api/conversationapi/model/request/message/AbstractMessageRequest.java
+++ src/main/java/com/sinch/sdk/api/conversationapi/model/request/message/AbstractMessageRequest.java
@@ -1,8 +1,14 @@
 package com.sinch.sdk.api.conversationapi.model.request.message;
 
 import com.sinch.sdk.model.conversationapi.AppMessage;
+import com.sinch.sdk.model.conversationapi.ChannelIdentities;
+import com.sinch.sdk.model.conversationapi.ChannelRecipientIdentity;
 import com.sinch.sdk.model.conversationapi.Recipient;
 import com.sinch.sdk.model.conversationapi.SendMessageRequest;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractMessageRequest<T, R> implements MessageRequest<T, R> {
@@ -21,8 +27,31 @@ public abstract class AbstractMessageRequest<T, R> implements MessageRequest<T, 
 
   @Override
   public T recipient(final Recipient recipient) {
-    request.setRecipient(recipient);
+    final Recipient existingRecipient = request.getRecipient();
+    if (existingRecipient == null) {
+      request.setRecipient(recipient);
+    } else {
+      final String contactId = recipient.getContactId();
+      if (contactId != null) {
+        existingRecipient.contactId(contactId);
+      }
+      final ChannelIdentities existingIdentifiedBy = existingRecipient.getIdentifiedBy();
+      if (existingIdentifiedBy == null) {
+        existingRecipient.setIdentifiedBy(recipient.getIdentifiedBy());
+      } else {
+        existingIdentifiedBy.setChannelIdentities(
+            Stream.concat(safeStream(existingIdentifiedBy), safeStream(recipient.getIdentifiedBy()))
+                .collect(Collectors.toList()));
+      }
+    }
     return (T) this;
+  }
+
+  private Stream<ChannelRecipientIdentity> safeStream(final ChannelIdentities identifiedBy) {
+    return Optional.ofNullable(identifiedBy)
+        .map(ChannelIdentities::getChannelIdentities)
+        .map(List::stream)
+        .orElse(Stream.empty());
   }
 
   protected AppMessage getAppMessage() {
